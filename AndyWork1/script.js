7998e0252aefbfac2c0967fb97ae9c5fca1b3b1a
lefelate <44091564+lefelate@users.noreply.github.com>
2021-11-02 22:28:34 +0000

    Add files via upload

--- AndyWork1/script.js
+++ AndyWork1/script.js
@@ -1,14 +1,23 @@
 const countEl=document.getElementById('count_el');
 
-setInterval(increment,2000);
-
 let count=0;
 
-function increment(){
-    count+=1;
+let increment =setInterval(() => {
+    count= count+1;
     countEl.innerText=count;
-    if( count==10){
-        count=0;
+    if(count===10){
+        count=10;
+        clearInterval(increment);
+        let decrement=setInterval(() => {
+            count--;
+           countEl.innerText=count;
+           if( count==0){
+               clearInterval(decrement);
+           }
+       }, 2000);
     }
     return count;
-}
+}, 2000);
+
+
+
